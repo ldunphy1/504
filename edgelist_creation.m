1617d5a10e68af3f67a3476d14c89bdfcdd44288
TrentWM <trent.w.montgomery@gmail.com>
2016-12-09 23:59:47 -0500

    Improved most of file thru use of built-in Matlab functions

--- edgelist_creation.m
+++ edgelist_creation.m
@@ -1,71 +1,39 @@
 function [] = edgelist_creation()
 %quick function to create random weighted edgelists for testing
-%right now just creates a few quick complete graphs to test
 %saves to csv file for reading into python program
 
-
-%create complete graphs
-A25_c = create_adjacency(25,25*24/2); %25 nodes, complete edges
-A50_c = create_adjacency(50,50*49/2); %50 nodes, complete edges
-A100_c = create_adjacency(100,100*99/2); %100 nodes, complete edges
-A200_c = create_adjacency(200,200*199/2); %200 nodes, complete edges
-
+%create graphs with different numbers of nodes
+A25_10 = create_adjacency(25,150,.1); %25 nodes, ~10% edges
+A25_50 = create_adjacency(25,150,.5); %25 nodes, ~50% edges
+A50_10 = create_adjacency(50,150,.1); %50 nodes
+A50_50 = create_adjacency(50,150,.5);
 
 %create edgelists
-E25_c = create_edgelist(A25_c);
-E50_c = create_edgelist(A50_c);
-E100_c = create_edgelist(A100_c);
-E200_c = create_edgelist(A200_c);
+E25_10 = create_edgelist(A25_10);
+E25_50 = create_edgelist(A25_50);
+E50_10 = create_edgelist(A50_10);
+E50_50 = create_edgelist(A50_50);
 
-csvwrite('E25_c.csv',E25_c);
-csvwrite('E50_c.csv',E50_c);
-csvwrite('E100_c.csv',E100_c);
-csvwrite('E200_c.csv',E200_c);
+%write to csv
+csvwrite('E25_10.csv',E25_10);
+csvwrite('E25_50.csv',E25_50);
+csvwrite('E50_10.csv',E50_10);
+csvwrite('E50_50.csv',E50_50);
 
 end
 
-function [A] = create_adjacency(num_nodes,num_edges)
-    max_edges = num_nodes*(num_nodes-1)/2;
-    
-    if num_edges > max_edges  
-        error('Too many edges -- more than complete graph')
-    else
-        A = randi(num_nodes,num_nodes);
-        A(logical(eye(size(A)))) = 0; %zero out the diagonal, no self-edges
-        
-        removals = max_edges - num_edges;
-        for i = 1:removals
-            %remove as many edges as requested by zeroeing weight
-            u = randi(num_nodes);
-            v = randi([1:u-1,u+1:num_nodes]); %avoid diagonal coordinates
-            A(u,v) = 0;
-        end
-    end
-end
-
-function [A] = remove_edges(A,num_removals)
-    
-    for i = 1:num_removals
-        u = randi(num_nodes);
-        v = randi([1:u-1,u+1:num_nodes]); %avoid diagonal coordinates
-        A(u,v) = 0;
-    end
-
+function [A] = create_adjacency(num_nodes,max_weight,density)
+%creates sparse adjacency matrix with an *approximate* density as specified
+%all elements are integers, graph is undirected (symmetric adjacency)
+    A = sprandsym(num_nodes, density );
+    A(A~=0) = randi(max_weight,nnz(A),1);
 end
 
 function [E] = create_edgelist(A)
-    E = zeros(nnz(A),3);
-    edge_cnt = 1;
-    num_nodes = size(A,1);
-    for i = 1:num_nodes
-        for j = 1:num_nodes
-            weight = A(i,j);
-            if weight~=0
-                E(edge_cnt,:) = [i,j,weight];
-                edge_cnt = edge_cnt +1;
-            end
-        end
-        
-    end
+%Takes adjacency matrix A as input, returns 3 column vector of edges
+%E = [start,end, weight]
+
+    [u,v,w] = find(A);
+    E = [u,v,w];
 
 end
