c77366c304126155612ba24fce8b032c8d449da3
TrentWM <trent.w.montgomery@gmail.com>
2016-12-10 16:06:09 -0500

    Generalized for graph creation of repeat versions of same specs.

--- edgelist_creation.m
+++ edgelist_creation.m
@@ -1,31 +1,36 @@
-function [] = edgelist_creation()
+function [] = edgelist_creation(num_graphs,num_nodes,density)
 %quick function to create random weighted edgelists for testing
 %saves to csv file for reading into python program
 
-%create graphs with different numbers of nodes
-A25_10 = create_adjacency(25,150,.1); %25 nodes, ~10% edges
-A25_50 = create_adjacency(25,150,.5); %25 nodes, ~50% edges
-A50_10 = create_adjacency(50,150,.1); %50 nodes
-A50_50 = create_adjacency(50,150,.5);
+%INPUT:
+%num_graphs: number of desired graph edgelists to produce
+%num_nodes: number of nodes in each graph (i.e. all will have same number)
+%density: int from 1 to 100 indicating the desired edge density pct of graph
 
-%create edgelists
-E25_10 = create_edgelist(A25_10);
-E25_50 = create_edgelist(A25_50);
-E50_10 = create_edgelist(A50_10);
-E50_50 = create_edgelist(A50_50);
+%OUTPUT:
+%each edgelist will be saved in a csv file with the name in the format of:
+% E[numnodes]_[density]_[id].  E.g. E25_10_1 is an edgelist for a 25 node,
+% 10 pct density graph and is given the ID 1.  ***Existing Files with the
+% same name will be overwritten***
 
-%write to csv
-csvwrite('E25_10.csv',E25_10);
-csvwrite('E25_50.csv',E25_50);
-csvwrite('E50_10.csv',E50_10);
-csvwrite('E50_50.csv',E50_50);
+    %create edgelists with desired number of nodes and density
+    E = cell(num_graphs,1);
+    for i = 1:num_graphs
+        A = create_adjacency(num_nodes,150,density);
+        E{i} = create_edgelist(A);
+    end
 
+    for i = 1:num_graphs
+        filename = strcat('E',num2str(num_nodes),'_',num2str(density),'_',num2str(i),'.csv');
+        csvwrite(filename,E{i})
+        
+    end
 end
 
 function [A] = create_adjacency(num_nodes,max_weight,density)
 %creates sparse adjacency matrix with an *approximate* density as specified
 %all elements are integers, graph is undirected (symmetric adjacency)
-    A = sprandsym(num_nodes, density );
+    A = sprandsym(num_nodes, density/100 );
     A(A~=0) = randi(max_weight,nnz(A),1);
 end
 
